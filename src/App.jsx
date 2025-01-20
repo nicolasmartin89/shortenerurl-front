62436c47ae67c7d0fb44bca07774d977c12f738a
Nicolas <nicolasmartin89@gmail.com>
2025-01-20 16:59:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-montserrat underline text-cyan-700">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
