26c1a92f9117f4b76f362eae9817fd12f3c19d26
Nicolas <nicolasmartin89@gmail.com>
2025-01-23 11:43:54 -0300

    Add Toaster notif.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import AboutPage from "./Components/AboutPage";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import RegisterPage from "./Components/RegisterPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Toaster position="bottom-center" />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
